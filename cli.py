267593c789b560ddcdfcb14f8c84c7883dc14048
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-19 16:42:29 -0400

    makes handle_user_input

--- cli.py
+++ cli.py
@@ -7,3 +7,16 @@ def start():
 
     user_input = input("Please enter you selection (1 - 3): ")
     handle_user_input(user_input)
+
+
+def handle_user_input(user_input):
+    if(user_input == 1):
+        sign_in()
+    elif(user_input == 1):
+        sign_up()
+    elif(user_input == 1):
+        exit_menu()
+    else:
+        print("Invalid selection.")
+        clear_screen()
+        start()
