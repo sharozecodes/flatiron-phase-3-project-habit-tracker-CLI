b8160788548d601ae9f4e046dc3cce90c68683e9
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-19 19:08:31 -0400

    implements simple-term-menu

--- lib/cli.py
+++ lib/cli.py
@@ -1,14 +1,19 @@
 import os
 from prettycli import red
+from simple_term_menu import TerminalMenu
 
 def start():
     print("Welcome to HABIT TRACKER\n")
-    print("1) Sign In\n")
-    print("2) Sign Up\n")
-    print("3) Exit\n")
+    options = ["Sign In", "Sign Up", "Exit"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+   
+    print(f"You have selected {options[menu_entry_index]}! ")
+    # print("2) Sign Up\n")
+    # print("3) Exit\n")
 
-    user_input = input("Please enter you selection (1 - 3): ")
-    handle_user_input(user_input)
+    # user_input = input("Please enter you selection (1 - 3): ")
+    # handle_user_input(user_input)
 
 
 def handle_user_input(user_input):
