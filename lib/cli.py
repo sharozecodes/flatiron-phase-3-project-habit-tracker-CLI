67674e849a8e0ff2d34c6ee159aefd1c36915c2a
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-19 19:11:30 -0400

    updated handle_user_input

--- lib/cli.py
+++ lib/cli.py
@@ -6,28 +6,17 @@ def start():
     print("Welcome to HABIT TRACKER\n")
     options = ["Sign In", "Sign Up", "Exit"]
     terminal_menu = TerminalMenu(options)
-    menu_entry_index = terminal_menu.show()
-   
-    print(f"You have selected {options[menu_entry_index]}! ")
-    # print("2) Sign Up\n")
-    # print("3) Exit\n")
-
-    # user_input = input("Please enter you selection (1 - 3): ")
-    # handle_user_input(user_input)
+    menu_index = terminal_menu.show()
+    handle_user_input(menu_index+1)
 
 
 def handle_user_input(user_input):
-    if(user_input == "1"):
+    if(user_input == 1):
         print("Option 1")
-    elif(user_input == "2"):
+    elif(user_input == 2):
         print("option 2")
-    elif(user_input == "3"):
+    elif(user_input == 3):
         clear_screen()
-        start()
-    else:
-        print(red("Invalid selection.\n"))
-        # clear_screen()
-        start()
     
 
 def clear_screen():
