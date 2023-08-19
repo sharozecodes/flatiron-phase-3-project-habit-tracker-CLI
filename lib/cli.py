5428470440d4e9ce39cbb544e9335580408b9f20
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-19 19:26:24 -0400

    implements ascii art for welcome screen

--- lib/cli.py
+++ lib/cli.py
@@ -2,8 +2,17 @@ import os
 from prettycli import red
 from simple_term_menu import TerminalMenu
 
+
 def start():
-    print("Welcome to HABIT TRACKER\n")
+    # print("\n\n\nWelcome to HABIT TRACKER\n".center(700))
+    print("""
+         _   _    _    ____ ___ _____   _____ ____      _    ____ _  _______ ____
+        | | | |  / \  | __ )_ _|_   _| |_   _|  _ \    / \  / ___| |/ / ____|  _ \\
+        | |_| | / _ \ |  _ \| |  | |     | | | |_) |  / _ \| |   | ' /|  _| | |_) |
+        |  _  |/ ___ \| |_) | |  | |     | | |  _ <  / ___ \ |___| . \| |___|  _ <
+        |_| |_/_/   \_\____/___| |_|     |_| |_| \_\/_/   \_\____|_|\_\_____|_| \_\\
+        """)
+
     options = ["Sign In", "Sign Up", "Exit"]
     terminal_menu = TerminalMenu(options)
     menu_index = terminal_menu.show()
@@ -22,5 +31,7 @@ def handle_user_input(user_input):
 def clear_screen():
     os.system("clear")
     
+def sign_in():
+    clear_screen()
 
 start()
