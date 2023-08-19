eff7dd64fb83624edc08d7b1b5a564128541c492
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-19 17:16:25 -0400

    implements start function

---
+++ lib/cli.py
@@ -0,0 +1,12 @@
+import os
+
+def start():
+    print("Welcome to HABIT TRACKER\n")
+    print("1) Sign In\n")
+    print("2) Sign Up\n")
+    print("3) Exit\n")
+
+    user_input = input("Please enter you selection (1 - 3): ")
+    handle_user_input(user_input)
+
+
