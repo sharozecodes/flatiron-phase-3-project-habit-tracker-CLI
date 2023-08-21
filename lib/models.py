461107efd65fcb5eabf50b1081886b58950530bf
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-21 13:50:33 -0400

    creates seed.py

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,10 @@
-
+from sqlalchemy import Column, Integer
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    
