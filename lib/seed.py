65c1e35ed8bc54efa4ca0675107e24d5d0f53a40
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-21 14:03:03 -0400

    database overwrites old entries with seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -4,16 +4,18 @@ from models import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///db/habittracker.db")
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///db/habittracker.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    session.query(User).delete()
+    
+    users = [
+        User(username='pable'),
+        User(username='pablo'),
+        User(username='pabla')
+    ]
 
-users = [
-    User(),
-    User(),
-    User()
-]
-
-session.bulk_save_objects(users)
-session.commit()
+    session.bulk_save_objects(users)
+    session.commit()
