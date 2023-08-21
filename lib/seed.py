0cc25fe839e406fb72bbb5730b2f0c67ed0fc528
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-21 13:55:11 -0400

    generates template for seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -1,5 +1,19 @@
-from faker import Faker
+# from faker import Faker
 import random
-
+from models import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///db/habittracker.db")
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+users = [
+    User(),
+    User(),
+    User()
+]
+
+session.bulk_save_objects(users)
+session.commit()
