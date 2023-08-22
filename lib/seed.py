0a35effd7ea4c98cdd105a5688bb21ed00a3d00f
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-21 23:07:21 -0400

    tests seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -1,6 +1,6 @@
 # from faker import Faker
 import random
-from models import User
+from models import User, Habit
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     session = Session()
 
     session.query(User).delete()
+    session.query(Habit).delete()
     
     users = [
         User(username='pable'),
@@ -18,5 +19,14 @@ if __name__ == '__main__':
     ]
 
     session.bulk_save_objects(users)
+    session.commit()
+
+    habits = [
+        Habit(title='adadaf'),
+        Habit(title='asfafa'),
+        Habit(title='sdvcscdsf')
+    ]
+
+    session.bulk_save_objects(habits)
     session.commit()
     session.close()
