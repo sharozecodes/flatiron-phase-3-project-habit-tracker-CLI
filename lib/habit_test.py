baded10aa0db6ae143ba1a1933cad98cc85354ea
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-27 14:29:33 -0400

    tests id for habit tester

--- lib/habit_test.py
+++ lib/habit_test.py
@@ -5,6 +5,11 @@ from simple_term_menu import TerminalMenu
 from queries import *
 import time
 
+engine = create_engine("sqlite:///db/habittracker.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
 def logo():
     os.system("clear")
     print(green("""
@@ -14,10 +19,35 @@ def logo():
         ██   ██ ██   ██ ██   ██ ██    ██           ██    ██           ██    ██    ██      ██   ██ 
         ██   ██ ██   ██ ██████  ██    ██           ██    ███████ ███████    ██    ███████ ██   ██
         """))
+def get_all_users():
+    return session.query(User).all()
 
+def get_user_id(user_ids):
+    while True:
+        logo()
+        user_id = input("Please enter the user id: ")
+        if user_id not in user_ids:
+            print(red("\ninvalid user id"))
+            time.sleep(1)
+            continue
+        else:
+            return user_id
+        
 def start():
-    pass
+    logo()
+    users = get_all_users()
+    user_ids = []
+    for user in users:
+        user_ids.append(str(user.id))
+
+    print(get_user_id(user_ids))
     
 
+    
 
-logo()
+    
+def main():
+    start()
+
+if __name__ == '__main__':
+    main()
